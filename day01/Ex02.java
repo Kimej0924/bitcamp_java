7784a1b7e620fdcb53a141ebdb561658608b91dd
Kimej0924 <54406178+Kimej0924@users.noreply.github.com>
2019-10-24 20:01:18 +0900

    Add files via upload

---
+++ day01/Ex02.java
@@ -0,0 +1,18 @@
+class Ex02
+{
+
+public static void main(String[] args){
+	System.out.println("문자열"+1234);
+	System.out.println(3.14+"문자열");
+	System.out.println("문자열"+1+2);	//문자열12
+	System.out.println("문자열"+(1+2));	//문자열3
+	System.out.println(3+4+"문자열");	//7문자열
+
+	System.out.println('한'+1);
+
+
+	
+}	
+
+
+}
